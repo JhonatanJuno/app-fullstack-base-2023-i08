27b73f033da5765ae8ab7342e2076a5e1fa29233
Jhonatan Juno <jhonatan_10_juno@hotmail.com>
2023-05-01 20:07:19 -0500

    Cambios de la Segunda clase

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -4,3 +4,11 @@ function SayHello(){
     let new_value = "Hello world!!!" + "\n" + current_value.value;
     document.getElementById("textarea_1").innerHTML = new_value;
 }
+
+var fun:any = SayHello;
+var numer = 21;
+var nombre = "Jhonatan"
+window.addEventListener("load", () => { 
+    fun();
+    SayHello()
+});
